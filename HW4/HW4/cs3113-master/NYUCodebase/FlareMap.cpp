73fe30ae4c200371cffb1784a55c23da5bc691d9
Leonardo Lamboglia <ll3087@nyu.edu>
2018-04-04 15:41:14 -0400

    Homework4

--- HW4/HW4/cs3113-master/NYUCodebase/FlareMap.cpp
+++ HW4/HW4/cs3113-master/NYUCodebase/FlareMap.cpp
@@ -61,10 +61,15 @@ bool FlareMap::ReadLayerData(std::ifstream &stream) {
 				std::string tile;
 				for(int x=0; x < mapWidth; x++) {
 					std::getline(lineStream, tile, ',');
-					unsigned char val =  (unsigned char)atoi(tile.c_str());
-					if(val > 0) {
-						mapData[y][x] = val-1;
-					} else {
+					unsigned int val =  (unsigned int)atoi(tile.c_str());
+					if(val == 1) {
+						mapData[y][x] = val;
+					}
+					else if (val > 0)
+					{
+						mapData[y][x] = val - 1;
+					}
+					else {
 						mapData[y][x] = 0;
 					}
 				}
@@ -115,7 +120,7 @@ void FlareMap::Load(const std::string fileName) {
 			}
 		} else if(line == "[layer]") {
 			ReadLayerData(infile);
-		} else if(line == "[ObjectsLayer]") {
+		} else if(line == "[objects]") {
 			ReadEntityData(infile);
 		}
 	}
--- HW4/HW4/cs3113-master/NYUCodebase/main.cpp
+++ HW4/HW4/cs3113-master/NYUCodebase/main.cpp
@@ -26,12 +26,14 @@ enum entityType { NONE, PHYSICAL, PLAYER, ENEMY, TEXT};
 bool play = false;
 float TEXXMAX = 384;
 float TEXYMAX = 256;
-
-float TILE_SIZE = .5;
+float TILE_SIZE = .1f;
 int TEXXNUM = TEXXMAX / 16.0f;
 int TEXYNUM = TEXYMAX / 16.0f;
 float TEXXSIZE = 1.0f / 24.0f;
 float TEXYSIZE = 1.0f / 16.0f;
+float FRICTION = 1.0f;
+float GRAVITY = -1.0f;
+Matrix VIEWMATRIX;
 
 class Vector3 {
 public:
@@ -63,27 +65,75 @@ struct gameObj
 	float* texCoords;
 	Matrix* model;
 	Matrix* view;
-	float height = .5f;
-	float width = .5f;
+	float height = .1f;
+	float width = .1f;
 	float x = 0.0f;
 	float y = 0.0f;
-	float maxy = 1.9f;
-	float maxx = 3.7f;
-	float speed;
+	Vector3 vel;
+	Vector3 acc;
 	bool alive = true;
-	entityType type = NONE;
+	bool ydowncol;
+	bool yupcol;
+	bool xleftcol;
+	bool xrightcol;
+	bool right;
+	bool left;
+	std::string type = "";
 	
-	gameObj(float newx, float newy, entityType typer)
+	gameObj(float newx, float newy, std::string typer)
 	{
 		model = new Matrix;
-		view = new Matrix;
+		view = &VIEWMATRIX;
 		x = newx;
 		y = newy;
+		if (typer == "TEXT")
+		{
+			height = .5f;
+			width = .5f;
+		}
 		alive = 1;
-		speed = 2;
-		height = (.25);
-		width = (.25);
+		acc.x = 0;
+		acc.y = 0;
+		vel.y = 0.0f;
+		vel.x = 0.0f;
 		type = typer;
+		ydowncol = false;
+		yupcol = false;
+		xleftcol = false;
+		xrightcol = false;
+	}
+
+	float lerp(float v0, float v1, float t) {
+		return (1.0 - t)*v0 + t * v1;
+	}
+
+	void update(float elapsed, gameObj* obj)
+	{
+		collision(obj, elapsed);
+		if (left)
+		{
+			vel.x = -.5f;
+		}
+		else if (right)
+		{
+			vel.x = .5f;
+		}
+		
+		if (!(xleftcol) && !(xrightcol))
+		{
+			vel.x = lerp(vel.x, 0.0f, elapsed * FRICTION);
+		}
+		else
+		{
+			bool col;
+		}
+		if (!ydowncol) {
+			vel.y = lerp(vel.y, GRAVITY, elapsed * .5f);
+		}
+		vel.x += acc.x * elapsed;
+		vel.y += acc.y * elapsed;
+		x += vel.x * elapsed;
+		y += vel.y * elapsed;
 	}
 
 	void changeTex(float texx, float texy, float texwidth, float texheight, float texxsize, float texysize)
@@ -98,7 +148,7 @@ struct gameObj
 	bool collision(gameObj* obj, float elapsed)
 	{
 		bool inrange = false;
-		if (alive && obj->alive)
+		if (alive && obj->alive && obj->type != type)
 		{
 			float paddleroof = obj->y + (obj->height / 2.0f);
 			float paddlefloor = obj->y - (obj->height / 2.0f);
@@ -112,18 +162,24 @@ struct gameObj
 			bool ballxpaddle = ((ballright >= paddleleft && ballleft <= paddleright) || (ballright >= paddleleft && ballleft < paddleleft) || (ballleft <= paddleright && ballright > paddleright));
 			inrange = (ballfloor <= paddleroof && ballxpaddle && ballroof >= paddlefloor);
 		}
+		if (inrange)
+		{
+			obj->alive = 0;
+		}
 
 		return inrange;
 	}
 
-	void draw(ShaderProgram* program, GLuint img)
+	//bool collisiony() {}
+
+	void draw(ShaderProgram* program, GLuint img, float elapsed)
 	{
 		if (alive)
 		{
 			model->Identity();
 			model->Translate(x, y, 0.0f);
 			model->Scale(width, height, 0);
-			if (type == ENEMY || type == TEXT )
+			if (type == "ENEMY" || type == "TEXT" )
 			{
 				model->Rotate(180.0f * (3.1415926f / 180.0f));
 			}
@@ -137,6 +193,8 @@ struct gameObj
 			glVertexAttribPointer(program->texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
 			glEnableVertexAttribArray(program->texCoordAttribute);
 			glDrawArrays(GL_TRIANGLES, 0, 6);
+			glDisableVertexAttribArray(program->positionAttribute);
+			glDisableVertexAttribArray(program->texCoordAttribute);
 		}
 	}
 };
@@ -178,7 +236,7 @@ struct text
 		int counterx = 0;
 		for (float ax = x + -((width) / 2); ax < x + ((width) / 2); ax += ((x + width) / 13))
 		{
-			alientext[counterx] = new gameObj(ax, y, TEXT);
+			alientext[counterx] = new gameObj(ax, y, "TEXT");
 			counterx++;
 		}
 	}
@@ -193,11 +251,11 @@ struct text
 		}
 	}
 
-	void draw(ShaderProgram* program)
+	void draw(ShaderProgram* program, float elapsed)
 	{
 		for(gameObj* n: alientext)
 		{
-			n->draw(program, img);
+			n->draw(program, img,elapsed);
 		}
 	}
 
@@ -212,6 +270,8 @@ struct worldObjs
 	FlareMap Map;
 	std::vector<float> objBlocks;
 	std::vector<float> texCoordData;
+	std::vector<gameObj> entities;
+	Matrix* view = &VIEWMATRIX;
 	int num = 0;
 
 	float textxcoords(int object)
@@ -219,6 +279,7 @@ struct worldObjs
 		float returnval = (float)(((int)object) % TEXXNUM) / (float)TEXXNUM;
 		return returnval;
 	}
+
 	float textycoords(int object)
 	{
 		float returnval = (float)(((int)object) / TEXYNUM) / (float)TEXYNUM;
@@ -226,7 +287,7 @@ struct worldObjs
 	}
 	void maptime()
 	{
-		Map.Load("Tiletimemapbutsmallnew.txt");
+		Map.Load("Tilemap.txt");
 	}
 	void Blocker()
 	{
@@ -237,23 +298,23 @@ struct worldObjs
 				if (val != 0) {
 					num += 6;
 					float u = ((float)(((int)Map.mapData[y][x]) % TEXXNUM)) / (float)TEXXNUM;
-					float v = ((float)(((int)Map.mapData[y][x]) / TEXYNUM)) / (float)TEXYNUM;
+					float v = ((float)(((int)Map.mapData[y][x]) / TEXXNUM)) / (float)TEXYNUM;
 
 					objBlocks.insert(objBlocks.end(), {
-						(TILE_SIZE * x) -offset, (TILE_SIZE * y) - offset,
-						(TILE_SIZE * x) - offset, ((TILE_SIZE * y) - TILE_SIZE) - offset,
-						((TILE_SIZE * x) + TILE_SIZE) - offset, ((TILE_SIZE * y) - TILE_SIZE) - offset,
-						(TILE_SIZE * x) - offset, (TILE_SIZE * y) - offset,
-						((TILE_SIZE * x) + TILE_SIZE) - offset, ((TILE_SIZE * y) - TILE_SIZE) - offset,
-						((TILE_SIZE * x) + TILE_SIZE)- offset, (TILE_SIZE * y)- offset
+						(TILE_SIZE * x) -offset, (-TILE_SIZE * y) - offset,
+						(TILE_SIZE * x) - offset, ((-TILE_SIZE * y) - TILE_SIZE) - offset,
+						((TILE_SIZE * x) + TILE_SIZE) - offset, ((-TILE_SIZE * y) - TILE_SIZE) - offset,
+						(TILE_SIZE * x) - offset, (-TILE_SIZE * y) - offset,
+						((TILE_SIZE * x) + TILE_SIZE) - offset, ((-TILE_SIZE * y) - TILE_SIZE) - offset,
+						((TILE_SIZE * x) + TILE_SIZE)- offset, (-TILE_SIZE * y)- offset
 						});
 
 					texCoordData.insert(texCoordData.end(), {
 						u, v,
-						u, v - (TEXYSIZE),
-						u + TEXXSIZE, v - (TEXYSIZE),
+						u, v + (TEXYSIZE),
+						u + TEXXSIZE, v + (TEXYSIZE),
 						u, v,
-						u + TEXXSIZE, v - (TEXYSIZE),
+						u + TEXXSIZE, v + (TEXYSIZE),
 						u + TEXXSIZE, v
 						});
 				}
@@ -261,20 +322,151 @@ struct worldObjs
 		}
 	}
 
+	void collisionx(gameObj* obj)
+	{
+		int xvals = (int)(obj->x/ TILE_SIZE);
+		int yvals = (int)(-obj->y / TILE_SIZE);
+		//worldToTileCoordinates(obj->x, obj->y, xvals, yvals);
+		bool leftcol = false;
+		bool rightcol = false;
+
+		for (int y = 0; y < Map.mapHeight; y++)
+		{
+			for (int x = 0; x < Map.mapWidth; x++)
+			{
+				int val = Map.mapData[y][x];
+				if (val != 0 && val != 20 && val != 21 && val != 22 && 23 && val != 44 && val != 45 && val != 46 && val != 47 && val != 68 && val != 69 && val != 70 && val != 71 && val != 92 && val != 93 && val != 94 && val != 95 && val != 117 && val != 118 && val != 141 && val != 142 && val != 165 && val != 166) {
+					//bool fuck = ((obj->x - fabs(obj->width / 2)) <= (TILE_SIZE * x) + TILE_SIZE && xvals >= x && yvals == y);
+					if ((obj->x - fabs(obj->width / 2)) <= (TILE_SIZE * x) + TILE_SIZE && xvals >= x && yvals == y)//((obj->y + fabs(obj->height / 2)) <= (-TILE_SIZE * y ) && (obj->y - fabs(obj->height/2) >= (-TILE_SIZE *y + TILE_SIZE ))))
+					{
+						obj->vel.x = 0;
+						obj->x += fabs(((obj->x - (fabs(obj->width) / 2))) - ((TILE_SIZE * x) + TILE_SIZE)) + 0.0000000001f;
+						leftcol = true;
+					}
+					if ((obj->x + fabs(obj->width / 2)) >= (TILE_SIZE * x) && xvals <= x && yvals == y)//(obj->y - (obj->height / 2)) == (-TILE_SIZE * y - TILE_SIZE))
+					{
+						obj->vel.x = 0;
+						obj->x -= fabs((obj->x + (fabs(obj->width) / 2))) - ((TILE_SIZE * x)) + 0.0000000001f;
+						rightcol = true;
+					}
+				}
+			}
+		}
+		obj->xleftcol = leftcol;
+		obj->xrightcol = rightcol;
+	}
+
+	void collisiony(gameObj* obj)
+	{
+		int xvals = (int)(obj->x / TILE_SIZE);
+		int yvals = (int)(-obj->y / TILE_SIZE);
+		bool downcol = false;
+		bool upcol = false;
+		for (int y = 0; y < Map.mapHeight; y++)
+		{
+			for (int x = 0; x < Map.mapWidth; x++)
+			{
+				int val = Map.mapData[y][x];
+				if (val != 0 && val != 20 && val!= 21 && val != 22 && 23 && val != 44 && val != 45 && val != 46 && val != 47 && val != 68 && val != 69 && val != 70 && val != 71 && val != 92 && val != 93 && val != 94 && val != 95 && val != 117 && val != 118 && val != 141 && val != 142 && val != 165 && val != 166) {
+					if ((obj->y - (obj->height / 2)) <= (-TILE_SIZE * y) && xvals == x && obj->y + (obj->height/2) > (-TILE_SIZE * y) )
+					{
+						obj->vel.y = 0;
+						obj->y += fabs((obj->y - (fabs(obj->width) / 2)) - (-TILE_SIZE * y)) + .0000001f;
+						downcol = true;
+					}
+
+					else if ((obj->y + (obj->height / 2)) >= (-TILE_SIZE * y) - TILE_SIZE && xvals == x && obj->y - (obj->height / 2) < (-TILE_SIZE * y) - TILE_SIZE)
+					{
+						obj->vel.y = 0;
+						obj->y -= fabs((obj->y + (fabs(obj->width) / 2)) - ((-TILE_SIZE * y))- TILE_SIZE) - 0.0000001f;
+						upcol = true;
+					}
+				}
+			}
+		}
+		obj->ydowncol = downcol;
+		obj->yupcol = upcol;
+	}
+
+	void docollisions(gameObj* obj)
+	{
+		collisionx(obj);
+		collisiony(obj);
+	}
+
+	void entityCollisions()
+	{
+		for (int i = 0; i< 2; i++)
+		{
+			docollisions(&entities[i]);
+		}
+	}
+
 	void create()
 	{
 		maptime();
 		Blocker();
+		entitygenerate();
 	}
 
 	void drawer(ShaderProgram* program, GLuint img)
 	{
+		Matrix model;
+
+		program->SetModelMatrix(model);
+		program->SetViewMatrix(*view);
 		glBindTexture(GL_TEXTURE_2D, img);
 		glVertexAttribPointer(program->positionAttribute, 2, GL_FLOAT, false, 0, &objBlocks[0]);
 		glEnableVertexAttribArray(program->positionAttribute);
 		glVertexAttribPointer(program->texCoordAttribute, 2, GL_FLOAT, false, 0, &texCoordData[0]);
 		glEnableVertexAttribArray(program->texCoordAttribute);
 		glDrawArrays(GL_TRIANGLES, 0, num);
+		glDisableVertexAttribArray(program->positionAttribute);
+		glDisableVertexAttribArray(program->texCoordAttribute);
+	}
+
+	void entitydrawer(ShaderProgram* program, GLuint img, float elapsed)
+	{
+		for (int i = 0; i< 2; i++)
+		{
+			entities[i].draw(program, img,elapsed);
+		}
+	}
+
+	void entityupdater(float elapsed)
+	{
+		for (int i = 0; i< 2; i++)
+		{
+			entities[i].update(elapsed, &entities[i]);
+		}
+	}
+
+	void entitygenerate()
+	{
+		int p = 0;
+		float u;
+		float v;
+		for (FlareMapEntity i : Map.entities)
+		{
+			if (i.type == "Enemy") {
+				u = ((float)(((int)90) % TEXXNUM)) / (float)TEXXNUM;
+				v = ((float)(((int)90) / TEXXNUM)) / (float)TEXYNUM;
+			}
+			else if (i.type == "Ally") {
+				u = ((float)(((int)42) % TEXXNUM)) / (float)TEXXNUM;
+				v = ((float)(((int)42) / TEXXNUM)) / (float)TEXYNUM;
+			}
+			gameObj newObj = *(new gameObj(i.x*TILE_SIZE,i.y*-TILE_SIZE, i.type));
+			newObj.texCoords = new float[12]{
+				u, v,
+				u, v + (TEXYSIZE),
+				u + TEXXSIZE, v + (TEXYSIZE),
+				u, v,
+				u + TEXXSIZE, v + (TEXYSIZE),
+				u + TEXXSIZE, v
+			};
+  			entities.push_back(newObj);
+		}
 	}
 
 };
@@ -313,7 +505,6 @@ int main(int argc, char *argv[])
 	program.Load(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
 	Matrix projectionMatrix;
 	Matrix playerModelMatrix;
-	Matrix playerViewMatrix;
 	projectionMatrix.SetOrthoProjection(-3.55, 3.55, -2.0f, 2.0f, -1.0f, 1.0f);
 	float lastFrameTicks = 0.0f;
 	GLuint player = LoadTexture(RESOURCE_FOLDER"dirt-tiles.png");
@@ -329,6 +520,7 @@ int main(int argc, char *argv[])
 	glClearColor(.4f, .5f, .4f, 1.0f);
 	worldObjs ZaWarudo;
 	ZaWarudo.create();
+	program.SetProjectionMatrix(projectionMatrix);
 	while (!done) {
 		float ticks = (float)SDL_GetTicks() / 1000.0f;
 		float elapsed = ticks - lastFrameTicks;
@@ -342,27 +534,50 @@ int main(int argc, char *argv[])
 			}
 			if (play)
 			{
-				if (keys[SDL_SCANCODE_LEFT] && keys[SDL_SCANCODE_RIGHT] && keys[SDL_SCANCODE_SPACE] && play)
-				{
-				}
-				else if (keys[SDL_SCANCODE_LEFT]&& keys[SDL_SCANCODE_SPACE] && play)
-				{
-				}
-				else if (keys[SDL_SCANCODE_RIGHT] && keys[SDL_SCANCODE_SPACE] && play)
-				{
-				}
-				if (keys[SDL_SCANCODE_LEFT] && keys[SDL_SCANCODE_RIGHT])
-				{
-				}
 				if (keys[SDL_SCANCODE_LEFT])
 				{
+					if (keys[SDL_SCANCODE_SPACE] && play)
+					{
+						if (ZaWarudo.entities[1].ydowncol)
+						{
+							ZaWarudo.entities[1].vel.y += .2f;
+						}
+					}
+					ZaWarudo.entities[1].left = true;
+					ZaWarudo.entities[1].right = false;
 				}
-				if (keys[SDL_SCANCODE_RIGHT])
+				else if (keys[SDL_SCANCODE_RIGHT])
 				{
+					if (keys[SDL_SCANCODE_SPACE] && play)
+					{
+						if (ZaWarudo.entities[1].ydowncol)
+						{
+							ZaWarudo.entities[1].vel.y += .2f;
+						}
+					}
+					ZaWarudo.entities[1].left = false;
+					ZaWarudo.entities[1].right = true;
 				}
-				else if (keys[SDL_SCANCODE_SPACE] && play)
+				else
 				{
+					if (keys[SDL_SCANCODE_SPACE] && play)
+					{
+						if (ZaWarudo.entities[1].ydowncol)
+						{
+							ZaWarudo.entities[1].vel.y += .2f;
+						}
+					}
+					ZaWarudo.entities[1].left = false;
+					ZaWarudo.entities[1].right = false;
 				}
+				if (keys[SDL_SCANCODE_SPACE] && play)
+				{
+					if (ZaWarudo.entities[1].ydowncol)
+					{
+						ZaWarudo.entities[1].vel.y += .2f;
+					}
+				}
+				
 			}
 			else
 			{
@@ -372,18 +587,25 @@ int main(int argc, char *argv[])
 				}
 			}
 		}
-		program.SetProjectionMatrix(projectionMatrix);
 		glClearColor(.4f, .5f, .4f, 1.0f);
 		if (play)
 		{
+			ZaWarudo.entityupdater(elapsed);
+			ZaWarudo.entityCollisions();
+			ZaWarudo.entities[1].collision(&ZaWarudo.entities[0], elapsed);
+			VIEWMATRIX.Identity();
+			VIEWMATRIX.Scale(1 / TILE_SIZE, 1 / TILE_SIZE, 1);
+			VIEWMATRIX.Translate(-ZaWarudo.entities[1].x, -ZaWarudo.entities[1].y, (0.0f));
+			program.SetViewMatrix(VIEWMATRIX);
 			ZaWarudo.drawer(&program, player);
+			ZaWarudo.entitydrawer(&program, player,elapsed);
 			glDisableVertexAttribArray(program.positionAttribute);
 			glDisableVertexAttribArray(program.texCoordAttribute);
 			SDL_GL_SwapWindow(displayWindow);
 		}
 		else
 		{
-			starting.draw(&program);
+			starting.draw(&program,elapsed);
 			glClearColor(.4f, .5f, .4f, 1.0f);
 			SDL_GL_SwapWindow(displayWindow);
 		}
